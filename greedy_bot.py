2fc23fc0a83d509daaaf2b31d9e0497b3cea5fd6
Alec Noble <aljnoble@ucsc.edu>
2015-04-08 16:52:48 -0700

    Stuck on uniform_bot and greedy_bot

--- greedy_bot.py
+++ greedy_bot.py
@@ -1,4 +1,24 @@
 def think(state, quip):
-    return state.get_moves()[0]
+    best_action = None
+    max_gains = 0
+
+    for move in state.get_moves():
+        curr_move = state.apply_move(move)
+        curr_score = state.get_score()
+
+        if best_action is None:
+            best_action = curr_move
+            max_gains = curr_score
+
+        elif max_gains < curr_score:
+            best_action = curr_move
+            max_gains = curr_score
+
+        else:
+            pass
+
+        # TODO: This is doing the exact same thing as uniform_bot, maybe I'm missing a key concept here.
+        state.apply_move(best_action)
+
 
 __author__ = 'Alec Noble and Delmy Reyes'
