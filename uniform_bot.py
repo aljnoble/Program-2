5ed2383dabd65fb99ec18341f8da9a937b1e6312
Alec Noble <aljnoble@ucsc.edu>
2015-04-08 16:36:37 -0700

    Stuck on uniform_bot

--- uniform_bot.py
+++ uniform_bot.py
@@ -7,9 +7,11 @@ def think(state, quip):
     random_move = random.choice(moves)
     print random_move
     print random_move[0], random_move[1]
+
     """ I have no idea why the below code freaks out about iterating over 'NoneType' when
     it is neither iterating or of type None."""
     # TODO: Fix this. It should be the last bit for the easy bot.
+
     state.apply_move(random_move)
 
 
